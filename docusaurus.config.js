8cdf567955e215aa4e22ee06255760e7a4afce48
Yudhvir Singh Godara <godarayudhvir@hotmail.com>
2022-11-09 02:48:46 +0530

    Updated Logo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -60,7 +60,7 @@ const config = {
         title: 'Docs by Cyber Alliance',
         logo: {
           alt: 'Docs by Cyber Alliance Logo',
-          src: 'img/logo.svg',
+          src: 'img/logo.png',
         },
         items: [
           {
